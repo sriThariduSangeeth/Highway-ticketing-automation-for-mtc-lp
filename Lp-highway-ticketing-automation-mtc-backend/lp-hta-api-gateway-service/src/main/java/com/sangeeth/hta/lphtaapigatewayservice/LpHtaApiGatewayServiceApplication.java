e4b6714c0a14a28edfbc4fe45dcc31ca23a0490f
sriThariduSangeeth <dtsangeeth@gmail.com>
2021-02-10 17:54:46 +0530

    add api gateway

---
+++ Lp-highway-ticketing-automation-mtc-backend/lp-hta-api-gateway-service/src/main/java/com/sangeeth/hta/lphtaapigatewayservice/LpHtaApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sangeeth.hta.lphtaapigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpHtaApiGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LpHtaApiGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ Lp-highway-ticketing-automation-mtc-backend/lp-hta-api-gateway-service/src/test/java/com/sangeeth/hta/lphtaapigatewayservice/LpHtaApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangeeth.hta.lphtaapigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpHtaApiGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
