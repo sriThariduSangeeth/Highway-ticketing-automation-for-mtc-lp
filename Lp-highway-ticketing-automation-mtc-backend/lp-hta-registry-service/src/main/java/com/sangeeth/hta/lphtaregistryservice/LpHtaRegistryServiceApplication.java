5a7a32a964a868dc72c3def5d16fabd0bbd5027c
sriThariduSangeeth <dtsangeeth@gmail.com>
2021-02-10 17:39:36 +0530

    service discover

---
+++ Lp-highway-ticketing-automation-mtc-backend/lp-hta-registry-service/src/main/java/com/sangeeth/hta/lphtaregistryservice/LpHtaRegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sangeeth.hta.lphtaregistryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpHtaRegistryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LpHtaRegistryServiceApplication.class, args);
+    }
+
+}
---
+++ Lp-highway-ticketing-automation-mtc-backend/lp-hta-registry-service/src/test/java/com/sangeeth/hta/lphtaregistryservice/LpHtaRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangeeth.hta.lphtaregistryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpHtaRegistryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
