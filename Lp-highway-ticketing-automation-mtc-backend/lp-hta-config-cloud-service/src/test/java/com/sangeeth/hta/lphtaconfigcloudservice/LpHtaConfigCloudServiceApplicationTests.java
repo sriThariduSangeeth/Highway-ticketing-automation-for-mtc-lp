d8eb92897215f34a7400e29edc833a88d8e87511
sriThariduSangeeth <dtsangeeth@gmail.com>
2021-02-10 18:08:54 +0530

    cloud config server

---
+++ Lp-highway-ticketing-automation-mtc-backend/lp-hta-config-cloud-service/src/test/java/com/sangeeth/hta/lphtaconfigcloudservice/LpHtaConfigCloudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangeeth.hta.lphtaconfigcloudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpHtaConfigCloudServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
