45206a1530cfeb85ca1ab8327891e2404d98c6d8
sriThariduSangeeth <dtsangeeth@gmail.com>
2021-02-11 18:39:54 +0530

    set config in vehicle

--- Lp-highway-ticketing-automation-mtc-backend/lp-hta-vehicle-service/src/main/java/com/sangeeth/hta/lphtavehicleservice/LpHtaVehicleServiceApplication.java
+++ Lp-highway-ticketing-automation-mtc-backend/lp-hta-vehicle-service/src/main/java/com/sangeeth/hta/lphtavehicleservice/LpHtaVehicleServiceApplication.java
@@ -3,10 +3,13 @@ package com.sangeeth.hta.lphtavehicleservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EntityScan(basePackages = "com.sangeeth.hta.commons.models.vehicle")
 public class LpHtaVehicleServiceApplication {
 
@@ -15,6 +18,7 @@ public class LpHtaVehicleServiceApplication {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
