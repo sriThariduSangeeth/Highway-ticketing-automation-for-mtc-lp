d2d48e47ec0627e31d5e215b7a91cf2176d18878
sriThariduSangeeth <dtsangeeth@gmail.com>
2021-02-12 02:20:33 +0530

    google drive api config

---
+++ Lp-highway-ticketing-automation-mtc-backend/lp-hta-vehicle-service/src/main/java/com/sangeeth/hta/lphtavehicleservice/services/GoogleDriveServiceImpl.java
@@ -0,0 +1,78 @@
+package com.sangeeth.hta.lphtavehicleservice.services;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.logging.Logger;
+
+/**
+ * @author dtsangeeth
+ * @created 12 / 02 / 2021
+ * @project lp-hta-vehicle-service
+ */
+@Slf4j
+public class GoogleDriveServiceImpl implements GoogleDriveService{
+
+    @Value("${google.service_account_email}")
+    private String serviceAccountEmail;
+
+    @Value("${google.application_name}")
+    private String applicationName;
+
+    @Value("${google.service_account_key}")
+    private String serviceAccountKey;
+
+    @Value("${google.folder_id}")
+    private String folderID;
+
+    public Drive getDriveService() {
+        Drive service = null;
+        try {
+            URL resource = GoogleDriveServiceImpl.class.getResource("/"+this.serviceAccountKey);
+            java.io.File key = Paths.get(resource.toURI()).toFile();
+            HttpTransport httpTransport = new NetHttpTransport();
+            JacksonFactory jsonFactory = new JacksonFactory();
+
+            GoogleCredential credential = new GoogleCredential.Builder().setTransport(httpTransport)
+                    .setJsonFactory(jsonFactory).setServiceAccountId(serviceAccountEmail)
+                    .setServiceAccountScopes(Collections.singleton(DriveScopes.DRIVE))
+                    .setServiceAccountPrivateKeyFromP12File(key).build();
+            service = new Drive.Builder(httpTransport, jsonFactory, credential).setApplicationName(applicationName)
+                    .setHttpRequestInitializer(credential).build();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+
+        return service;
+
+    }
+
+    @Override
+    public File upLoadFile(String fileName, String filePath, String mimeType) {
+        File file = new File();
+        try {
+            java.io.File fileUpload = new java.io.File(filePath);
+            com.google.api.services.drive.model.File fileMetadata = new com.google.api.services.drive.model.File();
+            fileMetadata.setMimeType(mimeType);
+            fileMetadata.setName(fileName);
+            fileMetadata.setParents(Collections.singletonList(folderID));
+            com.google.api.client.http.FileContent fileContent = new FileContent(mimeType, fileUpload);
+            file = getDriveService().files().create(fileMetadata, fileContent)
+                    .setFields("id,webContentLink,webViewLink").execute();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return file;
+    }
+}
